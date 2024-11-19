8587ec1cc68c756958ff2ce075ecce975886c208
khuggalsrn <ggalsrn@khu.ac.kr>
2024-11-19 19:09:12 +0900

    Update : order of UI logics

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.AssemblyQualifiedNameParser;
 using UnityEngine;
 using UnityEngine.UI;
 public class PlayerStatus : MonoBehaviour
@@ -11,11 +12,16 @@ public class PlayerStatus : MonoBehaviour
     [SerializeField] Text Cur_vel;
     [SerializeField] Text Cur_Acc;
     [SerializeField] Text HP;
+    [SerializeField] Slider HpBarSlider;
+    float MaxHP;
+    
+
     Rigidbody rigid;
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        MaxHP = GetComponent<PlayerInput>().MaxHP;
     }
 
     // Update is called once per frame
@@ -24,6 +30,13 @@ public class PlayerStatus : MonoBehaviour
         Max_vel.text = $"Max Velocity : {GetComponent<PlayerInput>().Max_Velocity}";
         Cur_vel.text = $"Your Velocity : {rigid.velocity.magnitude}";
         Cur_Acc.text = $"Cur Acceleration : {GetComponent<PlayerInput>().Cur_Acceleration}";
-        HP.text = $"HP : {GetComponent<PlayerInput>().HP} / {GetComponent<PlayerInput>().MaxHP} ";
+        float curHP = GetComponent<PlayerInput>().HP;
+        HP.text = $"HP : {curHP} / {MaxHP} ";
+        CheckHp(curHP);
+    }
+    public void CheckHp(float curHP) //*HP 갱신
+    {
+        if (HpBarSlider != null)
+            HpBarSlider.value = curHP / MaxHP;
     }
 }
